af0796ff4aa5ad15291f8a799e572de44c4ad52c
Ralf Mueller <ralf.mueller@zmaw.de>
2012-11-27 15:48:16 +0100

    Modularize plotting and limit it to my machine

--- rhopot.rb
+++ rhopot.rb
@@ -7,6 +7,68 @@ require 'jobqueue'
 require 'socket'
 require 'iconPlot'
 
+#==============================================================================
+def plot(ofile,experiment,secPlots,q,lock)
+  plotFile = 'thingol' == Socket.gethostname \
+           ? '/home/ram/src/git/icon/scripts/postprocessing/tools/icon_plot.ncl' \
+           : ENV['HOME'] +'/liz/icon/scripts/postprocessing/tools/icon_plot.ncl'
+  plotter  = 'thingol' == Socket.gethostname \
+           ? IconPlot.new(ENV['HOME']+'/local/bin/nclsh', plotFile, File.dirname(plotFile),'png','qiv',true,true) \
+           : IconPlot.new("/home/zmaw/m300064/local/bin/nclsh", plotFile, File.dirname(plotFile), 'png','display',true,true)
+  q.push {
+    im = plotter.scalarPlot(ofile,'T_'+     File.basename(ofile,'.nc'),'T',
+                            :tStrg => experiment, :bStrg => '" "',
+                            :hov => true,
+                            :minVar => -3.0,:maxVar => 3.0,:withLineLabels => true,
+                            :numLevs => 20,:rStrg => 'Temperature', :colormap => "BlWhRe")
+    lock.synchronize {(secPlots[experiment] ||= []) << im }
+ #  im = plotter.scalarPlot(experimentFiles[experiment][-1],'T_200m'+     File.basename(ofile,'.nc'),'T',
+ #                          :tStrg => experiment, :bStrg => '" "',:maskName => "wet_c",
+ #                          :levIndex => 6,
+ #                          :rStrg => 'Temperature')
+ #  lock.synchronize {mapPlots << im }
+ #  im = plotter.scalarPlot(experimentFiles[experiment][-1],'T_100m'+     File.basename(ofile,'.nc'),'T',
+ #                          :tStrg => experiment, :bStrg => '" "',:maskName => "wet_c",
+ #                          :levIndex => 12,
+ #                          :rStrg => 'Temperature')
+ #  lock.synchronize {mapPlots << im }
+  }
+  q.push {
+    im =  plotter.scalarPlot(ofile,'S_'+     File.basename(ofile,'.nc'),'S',
+                             :tStrg => experiment, :bStrg => '" "',
+                             :hov => true,
+                             :minVar => -0.2,:maxVar => 0.2,:withLineLabels => true,
+                             :numLevs => 16,:rStrg => 'Salinity', :colormap => "BlWhRe")
+    lock.synchronize {(secPlots[experiment] ||= []) << im }
+  }
+  q.push {
+    im = plotter.scalarPlot(ofile,'rhopot_'+File.basename(ofile,'.nc'),'rhopot',
+                            :tStrg => experiment, :bStrg => '"  "',
+                            :hov => true,
+                            :minVar => -0.6,:maxVar => 0.6,:withLineLabels => true,
+                            :numLevs => 24,:rStrg => 'Pot.Density', :colormap => "BlWhRe")
+    lock.synchronize {(secPlots[experiment] ||= []) << im }
+  }
+end
+#==============================================================================
+def cropPlots(secPlots)
+  sq = SystemJobs.new
+  cropfiles = []
+  secPlots.each {|exp,files|
+    files.each {|sp|
+      cropfile = "crop_#{File.basename(sp)}"
+      cmd      = "convert -resize 60%  -crop 650x560+50+50 #{sp} #{cropfile}"
+      puts cmd
+      sq.push(cmd)
+      cropfiles << cropfile
+    }
+  }
+  sq.run
+  system("convert +append #{(cropfiles.grep(/crop_T/) + cropfiles.grep(/crop_S/) + cropfiles.grep(/crop_rho/)).join(' ')} exp.png") #if 'thingol' == Socket.gethostname
+  #system("display #{cropfiles.join(' ')}") #if 'thingol' == Socket.gethostname
+end
+#==============================================================================
+#==============================================================================
 # check input
 if ARGV[0].nil?
   warn "no input files given"
@@ -14,47 +76,56 @@ if ARGV[0].nil?
 end
 
 files     = ( ARGV.size > 1 ) ? ARGV : Dir.glob(ARGV[0])
+maskFile  = ENV["MASK"].nil? ? "mask.nc" : ENV["MASK"]
 # check files
 files.each {|file|
   warn "Cannot read file '#{file}'" unless File.exist?(file)
 }
-
-q         = JobQueue.new([JobQueue.maxnumber_of_processors,8].min)
+unless File.exist?(maskFile) 
+  warn "Cannot open maskfile '#{maskFile}'"
+  exit -1
+end
+#==============================================================================
+q         = JobQueue.new([JobQueue.maxnumber_of_processors,16].min)
 lock      = Mutex.new
-#maskFile  = "mask-L40.nc"
-maskFile  = "mask.nc"
-diff2init = true
 
 Cdp.setCDO
+Cdp.setDebug
+Cdo.forceOutput = false
+Cdo.debug       = true
+diff2init       = true
+def plot?
+  'thingol' == Socket.gethostname
+end
+#==============================================================================
 
 # compute the experiments from the data directories and link the corresponding files
 gridfile, experimentFiles, experimentAnalyzedData = Cdp.splitFilesIntoExperiments(files)
-
-# compute meaked weight
-maskedAreaWeights = Cdp.manualMaskedAreaWeights("cell_area",gridfile,"wet_c",maskFile,"maskedAeraWeights.nc")
 
 # process the files
 #   start with selectiong the initial values from the first timestep
 experimentFiles.each {|experiment, files|
   q.push {
     initFile    = "initial_#{experiment}.nc"
+    puts "Computing initial value file: #{initFile}"
     # create a separate File with the initial values
-    unless File.exist?(initFile)
-      Cdo.selname('T,S',:in => "-seltimestep,1 #{files[0]}", :out => initFile,:options => '-r -f nc')
-    end
-    if File.exist?(initFile) and not Cdo.showname(:in => initFile).flatten.first.split(' ').include?("rhopot")
-      initRhopot = Cdo.rhopot(0,:in => initFile)
-      merged = Cdo.merge(:in => [initFile,initRhopot].join(' '))
+    if not File.exist?(initFile) or not Cdo.showname(:input => initFile).flatten.first.split(' ').include?("rhopot")
+      initTS     = Cdo.selname('T,S',:input => "-seltimestep,1 #{files[0]}",:options => '-r -f nc')
+      initRhopot = Cdo.rhopot(0,:input => initTS)
+      merged = Cdo.merge(:input => [initTS,initRhopot].join(' '))
       FileUtils.cp(merged,initFile)
     end
   }
 }
 q.run
+# compute meaked weight
+maskedAreaWeights = Cdp.maskedAreaWeights("cell_area",gridfile,"wet_c",maskFile,"maskedAeraWeightsFrom#{File.basename(maskFile)}")
+
 #   process the experiments results
 experimentFiles.each {|experiment, files|
   files.each {|file|
     q.push {
-      maskedYMeanFile = "masked_yearmean_#{File.basename(file)}"
+      maskedYMeanFile = "masked_#{File.basename(file)}"
       fldmeanFile     = "fldmean_#{File.basename(file)}"
       rhopotFile      = "rhopot_#{File.basename(file)}"
       mergedFile      = "T-S-rhopot_#{File.basename(file)}"
@@ -62,21 +133,21 @@ experimentFiles.each {|experiment, files|
       initFile        = "initial_#{experiment}.nc"
 
       unless File.exist?(maskedYMeanFile)
-        Cdo.div(:in => " -yearmean -selname,T,S #{file} #{maskFile}",:out => maskedYMeanFile)
+        Cdo.div(:input => " -selname,T,S #{file} #{maskFile}",:output => maskedYMeanFile)
       end
       # compute rhopot
       unless File.exist?(rhopotFile)
-        Cdo.rhopot(0,:in => maskedYMeanFile,:out => rhopotFile)
+        Cdo.rhopot(0,:input => maskedYMeanFile,:output => rhopotFile)
       end
 
       unless File.exist?(mergedFile)
-        Cdo.merge(:in => [maskedYMeanFile,rhopotFile].join(' '), :out => mergedFile)
+        Cdo.merge(:input => [maskedYMeanFile,rhopotFile].join(' '), :output => mergedFile)
       end
       unless File.exist?(diffFile)
-        Cdo.sub(:in => [mergedFile,initFile].join(' '),:out => diffFile)
+        Cdo.sub(:input => [mergedFile,initFile].join(' '),:output => diffFile)
       end
       unless File.exist?(fldmeanFile)
-        Cdo.fldsum(:in => "-mul #{diffFile} #{maskedAreaWeights}", :out => fldmeanFile,:options => '-r -f nc')
+        Cdo.fldsum(:input => "-mul #{diffFile} #{maskedAreaWeights}", :output => fldmeanFile,:options => '-r -f nc')
       end
       lock.synchronize {experimentAnalyzedData[experiment] << fldmeanFile }
     }
@@ -86,43 +157,19 @@ q.run
 
 # merge all yearmean data (T,S,rhopot) into one file per experiment
 q.clear
-images = []
+secPlots = {}
 experimentAnalyzedData.each {|experiment,files|
-  tag   = diff2init ? 'diff2init' : ''
-  ofile = [experiment,'T-S-rhopot',tag].join('_') + '.nc'
-  unless File.exist?(ofile)
-    Cdo.cat(:in => files.sort.join(' '), :out => ofile, :options => '-r')
-  end
-  plotFile = 'thingol' == Socket.gethostname \
-           ? '/home/ram/src/git/icon/scripts/postprocessing/tools/icon_plot.ncl' \
-           : '../../scripts/postprocessing/tools/icon_plot.ncl'
-  plotter  = 'thingol' == Socket.gethostname \
-           ? IconPlot.new(ENV['HOME']+'/local/bin/nclsh', plotFile, File.dirname(plotFile),'png','qiv',true,true) \
-           : IconPlot.new('/sw/rhel55-x64/ncl-5.2.1/bin/ncl', plotFile, File.dirname(plotFile), 'ps','evince',true,true)
-  q.push {
-    im = plotter.scalarPlot(ofile,'T_'+     File.basename(ofile,'.nc'),'T',
-                            :tStrg => experiment, :bStrg => '" a"',
-                            :hov => true,
-                            :minVar => -1.0,:maxVar => 5.0,
-                            :numLevs => 24,:rStrg => 'Temperature', :colormap => "BlueDarkRed18")
-    lock.synchronize {images << im }
-  }
-  q.push {
-    im =  plotter.scalarPlot(ofile,'S_'+     File.basename(ofile,'.nc'),'S',
-                             :tStrg => experiment, :bStrg => '"a "',
-                             :hov => true,
-                             :minVar => -0.2,:maxVar => 0.2,
-                             :numLevs => 16,:rStrg => 'Salinity', :colormap => "BlueDarkRed18")
-    lock.synchronize {images << im }
-  }
-  q.push {
-    im = plotter.scalarPlot(ofile,'rhopot_'+File.basename(ofile,'.nc'),'rhopot',
-                            :tStrg => experiment, :bStrg => '"  d"',
-                            :hov => true,
-                            :minVar => -0.6,:maxVar => 0.6,
-                            :numLevs => 24,:rStrg => 'Pot.Density', :colormap => "BlueDarkRed18")
-    lock.synchronize {images << im }
-  }
+  tag      = diff2init ? 'diff2init' : ''
+  ofile    = [experiment,'T-S-rhopot',tag].join('_') + '.nc'
+
+  yearmean = "yearmean"
+  ymfile   = [yearmean,ofile].join("_")
+
+  Cdo.cat(:input => files.sort.join(' '), :output => ofile, :force => true)
+  Cdo.settunits('years',:input => "-yearmean #{ofile}", :output => ymfile,:force => true)
+
+  ofile = ymfile
+  plot(ofile,experiment,secPlots,q,lock) if plot?
 }
 q.run
-system("eog #{images.join(' ')}")
+cropPlots(secPlots) if plot?
