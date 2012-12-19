134c40275d5d1442373599d887fab17764bee27d
Ralf Mueller <ralf.mueller@zmaw.de>
2012-12-19 13:21:43 +0100

    Added splitting into nh + sh; use plotutils via GSL for interactive and file plotting

--- iceVolume.rb
+++ iceVolume.rb
@@ -4,7 +4,9 @@ require 'cdo'
 require 'cdp'
 require 'fileutils'
 require 'jobqueue'
+require 'gsl'
 
+#=============================================================================== 
 # check input
 if ARGV[0].nil?
   warn "no input files given"
@@ -20,13 +22,30 @@ files.each {|file|
 }
 
 q         = JobQueue.new([JobQueue.maxnumber_of_processors,20].min)
+p         = JobQueue.new([JobQueue.maxnumber_of_processors,20].min)
 lock      = Mutex.new
-
 #=============================================================================== 
 # setup of CDO on different machines
 Cdp.setCDO
 Cdp.setDebug
+Cdo.forceOutput = ! ENV['FORCE'].nil?
+Cdo.debug       = ! ENV['DEBUG'].nil?
+#=============================================================================== 
+#=============================================================================== 
+# helper method for plotting ice volume and extent for NH and SH
+def plot(nhIceVolume,shIceVolume,nhIceExtent,shIceExtent,oType=nil,oTag=nil)
+  volumeOutput, extentOutput = '',''
+  volumeOutput = "-T #{oType} > iceVolume_#{oTag}.#{oType}" unless (oType.nil? and oTag.nil?)
+  extentOutput = "-T #{oType} > iceExtent_#{oTag}.#{oType}" unless (oType.nil? and oTag.nil?)
 
+  size = nhIceVolume.size
+  GSL::Vector.graph([GSL::Vector.linspace(0,size-1,size),nhIceVolume],
+                    [GSL::Vector.linspace(0,size-1,size),shIceVolume],
+                    "-C -g 3 -X 'timesteps' -Y 'Ice Volume [km^3]' -L 'IceVolume (red:NH, green:SH)' #{volumeOutput}")
+  GSL::Vector.graph([GSL::Vector.linspace(0,size-1,size),nhIceExtent],
+                    [GSL::Vector.linspace(0,size-1,size),shIceExtent],
+                    "-C -g 3 -X 'timesteps' -Y 'Ice Extent [km^2]' -L 'IceExtent (red:NH, green:SH)' #{extentOutput}")
+end
 #=============================================================================== 
 # compute the experiments from the data directories and link the corresponding files
 #=============================================================================== 
@@ -41,31 +60,51 @@ experimentFiles.each {|experiment, files|
       iceDiagFile = "iceDiag_#{File.basename(file)}"
       volumeFile  = "iceVolume_#{File.basename(file)}"
       extentFile  = "iceExtent_#{File.basename(file)}"
+      nhFile      = "nh_#{File.basename(file)}"
+      shFile      = "sh_#{File.basename(file)}"
+
       Cdo.setname('ice_volume',
-                  :in => " -divc,1e9 -mul -mul -selname,#{iceHeight} #{file}  -selname,cell_area #{gridfile}  -selname,#{iceConcentration} #{file}",
-                  :out => volumeFile) unless File.exist?(volumeFile)
+                  :input => " -divc,1e9 -mul -mul -selname,#{iceHeight} #{file}  -selname,cell_area #{gridfile}  -selname,#{iceConcentration} #{file}",
+                  :output => volumeFile)
 
       Cdo.setname('ice_extent',
-                  :in => " -divc,1e6 -mul -selname,cell_area #{gridfile}  -selname,#{iceConcentration} #{file}",
-                  :out => extentFile) unless File.exist?(extentFile)
+                  :input => " -divc,1e6 -mul -selname,cell_area #{gridfile}  -gtc,0.15 -selname,#{iceConcentration} #{file}",
+                  :output => extentFile)
 
-      Cdo.merge(:in => [volumeFile,extentFile].join(' '),
-                :out => iceDiagFile) unless File.exist?(iceDiagFile)
+      Cdo.merge(:input => [volumeFile,extentFile].join(' '),
+                :output => iceDiagFile)
 
-      lock.synchronize {experimentAnalyzedData[experiment] << iceDiagFile }
+      # split the file in norther and sourtern hemisphere
+      Cdo.fldsum(:input => "-sellonlatbox,-180,180,50,90 #{iceDiagFile}",  :output => nhFile) 
+      Cdo.fldsum(:input => "-sellonlatbox,-180,180,-50,-90 #{iceDiagFile}",:output => shFile) 
+
+      lock.synchronize {experimentAnalyzedData[experiment] << [nhFile,shFile] }
     }
   }
 }
 q.run
-# merge all yearmean data (T,S,rhopot) into one file per experiment
+# merge data together for NH and SH
 q.clear
+iceOutputsfiles = []
 experimentAnalyzedData.each {|experiment,files|
-  q.push {
-  tag   = ''
-  ofile = [experiment,'iceDiagnostics',tag].join('_') + '.nc'
-  unless File.exist?(ofile)
-    Cdo.cat(:in => files.sort.join(' '), :out => ofile, :options => '-r')
-  end
+  files.transpose.each_with_index {|hemisphereFiles,i|
+    q.push {
+      tag   = ['nh','sh'][i]
+      ofile = [experiment,'iceDiagnostics',tag].join('_') + '.nc'
+      FileUtils.rm(ofile) if File.exist?(ofile)
+      Cdo.cat(:input => hemisphereFiles.sort.join(' '), :output => ofile, :options => '-r')
+      lock.synchronize{ iceOutputsfiles << ofile }
+    }
   }
 }
 q.run
+
+nhFile, shFile = iceOutputsfiles
+
+nhIceVolume = Cdo.readArray(nhFile,'ice_volume').flatten.to_gv
+shIceVolume = Cdo.readArray(shFile,'ice_volume').flatten.to_gv
+nhIceExtent = Cdo.readArray(nhFile,'ice_extent').flatten.to_gv
+shIceExtent = Cdo.readArray(shFile,'ice_extent').flatten.to_gv
+
+plot(nhIceVolume,shIceVolume,nhIceExtent,shIceExtent)
+#plot(nhIceVolume,shIceVolume,nhIceExtent,shIceExtent,'png','iceTest')
