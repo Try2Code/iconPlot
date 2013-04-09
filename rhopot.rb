833925541220b09fb4489b0cc917574082b3a7b3
Ralf Mueller <stark.dreamdetective@gmail.com>
2013-04-09 09:31:31 +0200

    Update for cdo-1.6.0

--- rhopot.rb
+++ rhopot.rb
@@ -9,6 +9,7 @@ require 'iconPlot'
 
 #==============================================================================
 # some helper methods =========================================================
+def dbg(msg); pp msg unless ENV['DEBUG'].nil? ; end
 def myPlotter
   plotFile = 'thingol' == Socket.gethostname \
            ? '/home/ram/src/git/icon/scripts/postprocessing/tools/icon_plot.ncl' \
@@ -43,7 +44,7 @@ def secPlot(ofile,experiment,secPlots,lock,plotDir=".")
                            :minVar => -0.2,:maxVar => 0.2,:withLines => false,:lStrg => 'S',
                            :numLevs => 16,:rStrg => 'Salinity', :colormap => "BlWhRe")
   lock.synchronize {(secPlots[experiment] ||= []) << im }
-  im = plotter.scalarPlot(ofile,plotDir+'rhopot_'+File.basename(ofile,'.nc'),'rhopot',
+  im = plotter.scalarPlot(ofile,plotDir+'rhopot_'+File.basename(ofile,'.nc'),'rhopoto',
                           :tStrg => title, :bStrg => '"  "',
                           :hov => true,
                           :minVar => -0.6,:maxVar => 0.6,:withLines => false,:lStrg => 'rhopot',
@@ -125,7 +126,13 @@ def computeRhopot(ifile,ofile=nil)
     Cdo.rhopot(0,:input => ifile,:output => ofile)
   else
     # remove all codes so that adisit can use names
-    system("ncatted -O -a code,,d,, #{ifile}")
+    #  use ncatted (fast in-place edit) + mv (new filename to mark, that code
+    #  attribute is removed)
+    if Cdo.showcode(:input => ifile)[0].split.map(&:to_i).reduce(0,:+) >= 0
+      cmd = "ncatted -O -a code,,d,, #{ifile}"
+      dbg(cmd)
+      puts IO.popen(cmd).read
+    end
     Cdo.rhopot(0,:input => "-adisit #{ifile}",:output => ofile)
   end
 end
@@ -180,10 +187,9 @@ experimentFiles.each {|experiment, files|
     puts "Computing initial value file: #{initFile}"
     # create a separate File with the initial values
     if not File.exist?(initFile) or not Cdo.showname(:input => initFile).flatten.first.split(' ').include?("rhopot")
-      initTS     = Cdo.selname('T,S',:input => "-seltimestep,1 #{files[0]}",:options => '-r -f nc')
-      initRhopot = computeRhopot(initTS)
-      merged = Cdo.merge(:input => [initTS,initRhopot].join(' '))
-      FileUtils.cp(merged,initFile)
+      initTS     = Cdo.selname('T,S',:input => "-seltimestep,1 #{files[0]}",:options => '-r -f nc',:output => "initTS_#{experiment}")
+      initRhopot = computeRhopot(initTS,"initRhopot_#{experiment}")
+      Cdo.merge(:input => [initTS,initRhopot].join(' '),:output => initFile)
     end
   }
 }
