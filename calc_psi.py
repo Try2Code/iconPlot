7fd35d6c2ce3c0d8e8612e51343d452cc24ddecd
Ralf Mueller <stark.dreamdetective@gmail.com>
2014-04-03 00:11:38 +0200

    Add options for levels and remapping

--- calc_psi.py
+++ calc_psi.py
@@ -22,32 +22,58 @@ cdo.debug  = 'DEBUG' in os.environ
 #   colormap = 'jet'        (see http://matplotlib.org/examples/color/colormaps_reference.html for more)
 # =======================================================================================
 # INPUT HANDLING ========================================================================
+if len(sys.argv) < 2:
+    print("Provide an input file")
+    exit(1)
+
 inputfile = sys.argv[1]
 # stop if file cannot be read in
 if not os.path.isfile(inputfile):
     print("Cannot read input: "+inputfile)
     exit(1)
 
-options = {'VAR': 'u_vint_acc','PLOT': 'psi.png','CMAP': 'jet'}
+options = {'VAR': 'u_vint_acc',
+           'REMAP': True,
+           'PLOT': 'psi.png',
+           'CMAP': 'jet',
+           'LEVELS': [-150,-100,-75,-50,-30,-20,-10,-5,0,5,10,20,30,50,75,100,150],
+           'AREA': 'global'}
 
 optsGiven = sys.argv[2:]
 for optVal in optsGiven:
-    key,value = optVal.split('=')
+    key,value    = optVal.split('=')
+    if 'LEVELS' == key:
+        value = value.split(',')
+        if 1 == len(value):
+            value = int(value[0])
+        else:
+            value = map(lambda x: float(x), value)
+    if 'REMAP' == key:
+        if value in ['false','False','FALSE','0']:
+            value = False
+
     options[key] = value
 
-varName  = options['VAR']
-plotfile = options['PLOT']
-colormap = options['CMAP']
+varName    = options['VAR']
+remapInput = options['REMAP']
+plotfile   = options['PLOT']
+colormap   = options['CMAP']
+levels     = options['LEVELS']
+area       = options['AREA']
 # =======================================================================================
 # DATA PREPARATION ======================================================================
 
+# remapcon to regular 1deg grid
 # replace missing value with zero for later summation
-ifile     = cdo.setmisstoc(0.0,input = inputfile)
+if remapInput:
+    ifile = cdo.setmisstoc(0.0,input = '-remapcon,r360x180 '+inputfile,options='-P 8')
+else:
+    ifile = cdo.setmisstoc(0.0,input = inputfile)
+
 file_h    = cdo.readCdf(ifile)
 var       = file_h.variables[varName]
-varData   = cdo.readArray(ifile,varName)
-varDataMa = cdo.readMaArray(cdo.div(input = '%s -sellevidx,1 -selname,wet_c %s'%(inputfile,inputfile)),varName)
-varDims   = file_h.variables[varName].dimensions
+varData   = var[:]
+varDims   = var.dimensions
 # read in dimensions: expectes is 2d with time axis (time,lat,lon)
 a         = map(lambda x: file_h.variables[x][:], varDims)
 #times, depth, lats, lons = a[0], a[1], a[2], a[3] # MPIOM psi input
@@ -74,10 +100,6 @@ if times.size > 1:
 #varData = varData[-1,0,:,:] # MPIOM psi input
 varData = varData[-1,:,:]
 
-# avoid longitude greater than 180
-if lons.max() > 180.0:
-    lons = lons - 360.0
-
 # =======================================================================================
 # CALC PSI ==============================================================================
 psi      = np.array(varData)
@@ -93,63 +115,43 @@ psi  = -psi * dist * 1.0e-6
 #psi  = psi * 1.0e-6 / 1025.0 # MPIOM psi input
 # =======================================================================================
 # PLOTTING ==============================================================================
-if False:
-# draw
-    im = imshow(varData,
-            origin='lower',
-            interpolation='nearest',
-            cmap=colormap,
-            aspect='equal', # auto for fill
-            extent=[lons.min(),lons.max(),lats.min(),lats.max()])
-    cb = colorbar(im)
-    cb.set_label('Transport [Sv]')
-else:
-    matplotlib.rcParams['contour.negative_linestyle'] = 'solid'
-    plt.figure()
-    lon2d, lat2d = np.meshgrid(lons, lats)
-    mapproj = bm.Basemap(projection='cyl', 
-                       llcrnrlat=math.floor(lats.min()), 
-                       llcrnrlon=math.floor(lons.min()),
-                       urcrnrlat=math.floor(lats.max()),
-                       urcrnrlon=math.floor(lons.max()))
-#   mapproj = bm.Basemap(projection='cyl', 
-#                      llcrnrlat=-90, 
-#                      llcrnrlon=-180,
-#                      urcrnrlat=90,
-#                      urcrnrlon=180)
-    mapproj.drawcoastlines(linewidth=.2)
-    mapproj.drawmapboundary(fill_color='0.99')
-    mapproj.drawparallels(np.array([-80,-60,-40,-20, 0, 20,40,60,80]), labels=[1,0,0,0],fontsize=8)
-    mapproj.drawmeridians(range(0,360,30), labels=[0,0,0,1],fontsize=8)
-    lonsPlot, latsPlot = mapproj(lon2d, lat2d)
+plt.figure()
 
-    plt.grid(True)
-#   CS   = plt.contourf(lonsPlot, latsPlot, psi, 20, cmap=colormap)
-    CS   = plt.contourf(lonsPlot, latsPlot, psi, 20, cmap=colormap)
-#   CS   = plt.contourf(lonsPlot, latsPlot, psi, 20, color='black')
-    CSBar = plt.contour(lonsPlot,
-                        latsPlot,
-                        psi,
-                        CS.levels[::2],
-                        colors = ('k',),
-                        linewidths = (1,),
-                        origin = 'lower')
-    plt.title('Listed colors (3 masked regions)')
-    plt.clabel(CSBar, fmt = '%2.1f', colors = 'w', fontsize=10)
-    CSBar.cmap.set_under('yellow')
-    CSBar.cmap.set_over('cyan')
+matplotlib.rcParams['contour.negative_linestyle'] = 'solid'
+lon2d, lat2d = np.meshgrid(lons, lats)
+mapproj = bm.Basemap(projection='cyl', 
+                     llcrnrlat=math.floor(lats.min()), 
+                     llcrnrlon=math.floor(lons.min()),
+                     urcrnrlat=math.floor(lats.max()),
+                     urcrnrlon=math.floor(lons.max()))
+mapproj.drawcoastlines(linewidth=.2)
+if 'global' == area:
+    mapproj.fillcontinents(color='grey',lake_color='k')
+mapproj.drawmapboundary(fill_color='0.99')
+#   mapproj.drawparallels(np.array([-80,-60,-40,-20, 0, 20,40,60,80]), labels=[1,0,0,0],fontsize=2)
+#   mapproj.drawmeridians(range(0,360,30), labels=[0,0,0,1],fontsize=8)
+lonsPlot, latsPlot = mapproj(lon2d, lat2d)
 
-# labeling
-xlabel('lon [deg]')
-ylabel('lat [deg]')
-title('Bar. stream function')
-tick_params(axis='x', labelsize=8)
-tick_params(axis='y', labelsize=8)
+# contour plot
+CS   = plt.contourf(lonsPlot, latsPlot, psi, 
+                    levels,
+                    extend='both',
+                    cmap=colormap)
+# contour lines
+CSBar = plt.contour(lonsPlot,
+                    latsPlot,
+                    psi,
+                    CS.levels[::1],
+                    colors = ('k',),
+                    linewidths = (0.5,),
+                    origin = 'lower')
 
-# Notice that the colorbar command gets all the information it
-# needs from the ContourSet object, CS3.
-plt.colorbar(CS)
-#   mapproj.drawlsmask(land_color='coral')
+# contour line labels
+plt.clabel(CSBar, fmt = '%2.1f', colors = 'k', fontsize=6)
+
+# colorbar
+plt.colorbar(CS,orientation='horizontal')
+
 plt.title("Bar. streamfunction form:\n"+inputfile,fontsize=10)
 
 savefig(plotfile)
